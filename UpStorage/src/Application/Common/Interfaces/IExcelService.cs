8d21e2fad559ea87306ecf3b4fa9b1a2ca978fdd
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-03-30 22:26:58 +0300

    Application Layer and CQRS were introduced.

---
+++ UpStorage/src/Application/Common/Interfaces/IExcelService.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.Models.Excel;
+
+namespace Application.Common.Interfaces
+{
+    public interface IExcelService
+    {
+        List<ExcelCityDto> ReadCities(ExcelBase64Dto excelDto);
+        object ReadCities(string excelBase64File);
+    }
+}
