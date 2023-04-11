431cca3a7e89b7eb972dcec61e9f52251a501179
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-04-11 22:21:07 +0300

    FluentValidations foundations. ValidationBehaviour was created.

--- UpStorage/src/Application/Features/Cities/Commands/Add/CityAddCommandHandler.cs
+++ UpStorage/src/Application/Features/Cities/Commands/Add/CityAddCommandHandler.cs
@@ -1,9 +1,7 @@
 ﻿using Application.Common.Interfaces;
 using Domain.Common;
 using Domain.Entities;
-using Domain.Extensions;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.Cities.Commands.Add
 {
@@ -18,15 +16,6 @@ namespace Application.Features.Cities.Commands.Add
 
         public async Task<Response<int>> Handle(CityAddCommand request, CancellationToken cancellationToken)
         {
-            if (!await _applicationDbContext.Countries.AnyAsync(x=>x.Id==request.CountryId, cancellationToken))
-            {
-                throw new ArgumentNullException(nameof(request.CountryId));
-            }
-
-            if (await _applicationDbContext.Cities.AnyAsync(x => x.Name.ToLower() == request.Name.ToLower(), cancellationToken))
-            {
-                throw new ArgumentNullException(nameof(request.Name));
-            }
             var city = new City()
             {
                 Name = request.Name,
