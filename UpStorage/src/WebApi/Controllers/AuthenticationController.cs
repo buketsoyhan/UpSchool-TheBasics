44b4d171e6f3b91d00440f0a92d21e0a06d66ee1
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-04-16 17:01:34 +0300

    User's register operation was completed.

---
+++ UpStorage/src/WebApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Features.Auth.Commands.Register;
+
+namespace WebApi.Controllers
+{
+    [Route("/api[controller]")]
+    [ApiController]
+    public class AuthenticationController : ApiControllerBase
+    {
+        [HttpPost("Register")]
+        public async Task<IActionResult> ReagisterAsync(AuthRegisterCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
