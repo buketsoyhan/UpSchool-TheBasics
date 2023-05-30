116583eedf06fa3f7e8a28775a4d2e047f6a2e58
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-05-30 23:51:46 +0300

    Password Generator's design was refactored with Semantic UI.

---
+++ UpStorage/src/Application/Features/Accounts/Queries/GetAll/AccountGetAllQuery.cs
@@ -0,0 +1,15 @@
+﻿using Application.Common.Models.General;
+using MediatR;
+
+public class AccountGetAllQuery : IRequest<PaginatedList<AccountGetAllDto>>
+{
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+
+    public AccountGetAllQuery(int pageNumber, int pageSize)
+    {
+        PageNumber = pageNumber;
+
+        PageSize = pageSize;
+    }
+}
--- react-upstorage/src/App.tsx
+++ react-upstorage/src/App.tsx
@@ -1,58 +1,63 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import './App.css'
-import NavBar from './components/NavBar.tsx'
-import PasswordGenerator from './utils/PasswordGenerator.ts'
-import GeneratePasswordDto from './types/GeneratePasswordDto.ts'
-import { ToastContainer, toast } from 'react-toastify'
+import NavBar from './components/NavBar.tsx';
+import PasswordGenerator from './utils/PasswordGenerator.ts';
+import GeneratePasswordDto from "./types/GeneratePasswordDto.ts";
+import { ToastContainer, toast } from 'react-toastify';
+import { Button, Card, Checkbox, Container, Form, Grid, Header, Icon, Input, Segment } from "semantic-ui-react";
 
 function App() {
+  const passwordGenerator = new PasswordGenerator();
+  const generatePasswordDto = new GeneratePasswordDto();
 
-  const [password, setPassword] = useState<string>("123456")
-  const passwordGenerator = new PasswordGenerator()
+  const [password, setPassword] = useState<string>("123456");
 
-  const [savedPasswords, setSavedPasswords] = useState<string[]>([])
+  const [savedPasswords, setSavedPasswords] = useState<string[]>([]);
 
   const [passwordLength, setPasswordLength] = useState<number>(12);
 
   const [includeNumbers, setIncludeNumbers] = useState<boolean>(true);
-  const [includeLowercase, setIncludeLowecase] = useState<boolean>(true);
+  const [includeLowercase, setIncludeLowercase] = useState<boolean>(true);
   const [includeUppercase, setIncludeUppercase] = useState<boolean>(false);
   const [includeSpecialChars, setIncludeSpecialChars] = useState<boolean>(false);
 
-  const myStyles = {
-    iconStyles: {
-      cursor: "pointer",
-    }
-  }
-
   useEffect(() => {
-    handleGenerate()
-  }, [passwordLength, includeNumbers,includeLowercase,includeUppercase,includeSpecialChars])
+
+    handleGenerate();
+
+  }, [passwordLength, includeNumbers, includeLowercase, includeUppercase, includeSpecialChars]);
 
   const handleGenerate = (): void => {
-    const generatePasswordDto = new GeneratePasswordDto()
 
     generatePasswordDto.Length = passwordLength;
-    generatePasswordDto.IncludeNumbers = includeNumbers
-    generatePasswordDto.IncludeLowerCharacters = includeLowercase
-    generatePasswordDto.IncludeUpperCharacters = includeUppercase
-    generatePasswordDto.IncludeSpecialCharacters = includeSpecialChars
+    generatePasswordDto.IncludeNumbers = includeNumbers;
+    generatePasswordDto.IncludeLowerCharacters = includeLowercase;
+    generatePasswordDto.IncludeUpperCharacters = includeUppercase;
+    generatePasswordDto.IncludeSpecialCharacters = includeSpecialChars;
 
-    const newPass = passwordGenerator.Generate(generatePasswordDto)
+    const newPass = passwordGenerator.Generate(generatePasswordDto);
 
-    setPassword(newPass)
+    console.log(newPass);
+
+    setPassword(newPass);
+
   }
 
   const handleSavePassword = () => {
 
-    const samePassword = savedPasswords.find(x => x === password)
+    const samePassword = savedPasswords.find(x => x === password);
 
-    if (!samePassword) setSavedPasswords((prevState) => [...prevState, password])
+    if (!samePassword)
+      setSavedPasswords([...savedPasswords, password]);
+
   }
 
   const handleSavedPasswordDelete = (selectedPass: string) => {
-    const newSavedPasswords = savedPasswords.filter((pass) => pass != selectedPass)
-    setSavedPasswords(newSavedPasswords)
+
+    const newSavedPasswords = savedPasswords.filter((pass) => pass !== selectedPass);
+
+    setSavedPasswords(newSavedPasswords);
+
   }
 
   const handleChange = (value: string) => {
@@ -62,70 +67,93 @@ function App() {
     //handleGenerate();
   }
 
-
   const handleCopyToClipBoard = () => {
-    navigator.clipboard.writeText(password)
-    toast("The selected password copied to the clipboard!")
+
+    navigator.clipboard.writeText(password);
+    toast("The selected password copied to the clipboard.");
+
   }
 
   return (
     <>
-      <NavBar />
       <ToastContainer />
-      <div className="container App">
-        <div className="card-header is-justify-content-center">
-          <h3 className="has-text-success is-size-2">Secure Password Generator</h3>
-        </div>
-        <div className="card" style={{ backgroundColor: "#ECF8F9" }}>
-          <div className="card-content">
-            <div className="media">
-              <div className="media-content">
-                <p className="is-size-4">{password}</p>
-              </div>
-              <div className="media-right">
-                <span style={myStyles.iconStyles} className="is-size-3" onClick={handleSavePassword}>📁</span>
-                <span style={myStyles.iconStyles} className="is-size-3" onClick={handleCopyToClipBoard}>📋</span>
-                <span style={myStyles.iconStyles} className="is-size-3" onClick={handleGenerate}>♻️</span>
-              </div>
-            </div>
-            <div className="content has-text-centered">
-              <div className="field">
-                <input id="passwordLengthSelector" type="range" step={1} min={6} max={35} className="input mr-3"
-                  value={passwordLength} onChange={(event) => handleChange(event.currentTarget.value)} />
-                <label htmlFor="passwordLengthSelector" style={{ fontSize: '24px', fontWeight: "bold" }}>{passwordLength}</label>
-              </div>
-              <div className="field">
-                <label className="checkbox mr-2">
-                  <input type="checkbox" className="mr-1" checked={includeNumbers} onChange={(e)=>setIncludeNumbers(e.currentTarget.checked)}/>
-                  Numbers
-                </label>
-                <label className="checkbox mr-2">
-                  <input type="checkbox" className="mr-1" checked={includeLowercase} onChange={(e)=>setIncludeLowecase(e.currentTarget.checked)}/>
-                  Lowercase
-                </label>
-                <label className="checkbox mr-2">
-                  <input type="checkbox" className="mr-1" checked={includeUppercase} onChange={(e)=>setIncludeUppercase(e.currentTarget.checked)}/>
-                  Uppercase
-                </label>
-                <label className="checkbox mr-2">
-                  <input type="checkbox" className="mr-1" checked={includeSpecialChars} onChange={(e)=>setIncludeSpecialChars(e.currentTarget.checked)}/>
-                  Special Chars
-                </label>
-              </div>
-              <ol className="list is-hoverable">
-                {savedPasswords.map((pass, index) => (
-                  <li className='list-item has-text-weight-bold' key={index} >
-                    {pass}
-                    <span style={myStyles.iconStyles} onClick={() => handleSavedPasswordDelete(pass)}>🗑️</span>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
-        </div>
-      </div>
+      <NavBar />
+      <Container className="App">
+        <Header as='h2' textAlign='center' color='green' style={{ fontSize: '36px', fontWeight: 'bold' }}>Secure Password Generator</Header>
+        <Segment raised style={{ backgroundColor: '#A2BEB9', boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2)', transition: '0.3s', color: '#173A3A' }}>
+
+          <Grid>
+            <Grid.Row columns={2}>
+              <Grid.Column textAlign='center'>
+                <Header as='h4'>{password}</Header>
+                <Button icon onClick={handleSavePassword}><Icon name='save' /></Button>
+                <Button icon onClick={handleCopyToClipBoard}><Icon name='copy' /></Button>
+                <Button icon onClick={handleGenerate}><Icon name='refresh' /></Button>
+              </Grid.Column>
+              <Grid.Column>
+                <Form>
+                  <Form.Field>
+                    <label>Password Length</label>
+                    <Input
+                      id="passwordLengthSelector"
+                      type="number"
+                      value={passwordLength}
+                      min={6}
+                      max={35}
+                      onChange={(_, data) => handleChange(data.value)}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Checkbox
+                      label="Include Numbers"
+                      checked={includeNumbers}
+                      onChange={(_, data) => setIncludeNumbers(data.checked || true)}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Checkbox
+                      label="Include Lowercase Characters"
+                      checked={includeLowercase}
+                      onChange={(_, data) => setIncludeLowercase(data.checked || true)}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Checkbox
+                      label="Include Uppercase Characters"
+                      checked={includeUppercase}
+                      onChange={(_, data) => setIncludeUppercase(data.checked || true)}
+                    />
+                  </Form.Field>
+                  <Form.Field>
+                    <Checkbox
+                      label="Include Special Characters"
+                      checked={includeSpecialChars}
+                      onChange={(_, data) => setIncludeSpecialChars(data.checked || true)}
+                    />
+                  </Form.Field>
+                </Form>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+
+          <Header as='h4' style={{ paddingBottom: "15px" }}>Saved Passwords:</Header>
+          <Card.Group itemsPerRow={4} stackable>
+            {savedPasswords.map((pass, index) => (
+              <Card key={index}>
+                <Card.Content>
+                  <Card.Header>{pass}</Card.Header>
+                  <Card.Description>
+                    <Button icon onClick={() => handleSavedPasswordDelete(pass)}><Icon name='trash' /></Button>
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        </Segment>
+      </Container>
     </>
   )
+
 }
 
 export default App
