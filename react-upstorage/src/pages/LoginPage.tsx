bd5c17597f4fa9ecbf5ee80f618643d4e0d5b3b9
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-06-23 00:05:20 +0300

    Google SignIn functionality added.

--- react-upstorage/src/pages/LoginPage.tsx
+++ react-upstorage/src/pages/LoginPage.tsx
@@ -1,78 +1,73 @@
-import { useContext, useState } from "react"
-import { AuthLoginCommand, LocalJwt } from "../types/AuthTypes"
-import { Button, Form, Grid, Header, Icon, Segment, Image } from "semantic-ui-react"
-import axios from "axios"
-import { getClaimsFromJwt } from "../utils/jwtHelper"
-import { toast } from "react-toastify"
-import { useNavigate } from "react-router-dom"
-import { AppUserContext } from "../context/StateContext"
+import { AuthLoginCommand, LocalJwt } from "../types/AuthTypes.ts";
+import React, { useContext, useState } from "react";
+import { Button, Form, Grid, Header, Icon, Image, Segment } from "semantic-ui-react";
+import api from "../utils/axiosInstance.ts";
+import { getClaimsFromJwt } from "../utils/jwtHelper.ts";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import logo from '/upstorage_logo_730_608.png';
+import { AppUserContext } from "../context/StateContext.tsx"; 
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+/*export type LoginPageProps = {
+
+}*/
 
 function LoginPage() {
 
-    const {setAppUser}=useContext(AppUserContext)
+    const { setAppUser } = useContext(AppUserContext);
 
-    const navigate=useNavigate();
+    const navigate = useNavigate();
 
-    const [authLoginCommand, setAuthLoginCommand] = useState<AuthLoginCommand>({ email: "", password: "" })
+    const [authLoginCommand, setAuthLoginCommand] = useState<AuthLoginCommand>({ email: "", password: "" });
 
     const handleSubmit = async (event: React.FormEvent) => {
+
         event.preventDefault();
 
-        const BASE_URL=import.meta.env.VITE_API_URL;
-        const LOGIN_URL=`${BASE_URL}/Authentication/login`;
-        const response = await axios.post("https://localhost:7109/api/Authentication/Login", authLoginCommand);
+        try {
+            const response = await api.post("/Authentication/Login", authLoginCommand);
 
-        console.log(response)
-        if (response.status === 200) {
-            const accessToken = response.data.accessToken;
-            const { uid,
-                email,
-                given_name,
-                family_name,
-            } = getClaimsFromJwt(accessToken)
+            if (response.status === 200) {
+                const accessToken = response.data.accessToken;
+                const { uid, email, given_name, family_name } = getClaimsFromJwt(accessToken);
+                const expires: string = response.data.expires;
 
-            const expires:string=response.data.expires;
+                setAppUser({ id: uid, email, firstName: given_name, lastName: family_name, expires, accessToken });
 
-            setAppUser({
-                id: uid,
-                email,
-                firstName: given_name,
-                lastName: family_name,
-                expires,
-                accessToken
-            })
+                const localJwt: LocalJwt = {
+                    accessToken,
+                    expires
+                }
 
-            const localJwt:LocalJwt={
-                accessToken,
-                expires
+                localStorage.setItem("upstorage_user", JSON.stringify(localJwt));
+                navigate("/");
+            } else {
+                toast.error(response.statusText);
             }
-
-            localStorage.setItem("upstorage_user", JSON.stringify(localJwt));
-            navigate("/")
-        }
-        else{
-            toast.error(response.statusText)
+        } catch (error) {
+            toast.error("Something went wrong!");
         }
     }
-    const handleInputChange = (inputName: string, inputValue: string) => {
-        if (inputName == "email") setAuthLoginCommand({ ...authLoginCommand, email: inputValue })
 
-        else
-            setAuthLoginCommand({ ...authLoginCommand, password: inputValue })
-
-        console.log(authLoginCommand);
-
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAuthLoginCommand({
+            ...authLoginCommand,
+            [event.target.name]: event.target.value
+        });
     }
+
     const onGoogleLoginClick = (e: React.FormEvent) => {
-        // Handle Google login
         e.preventDefault();
 
+        window.location.href = `${BASE_URL}/Authentication/GoogleSignInStart`;
     };
 
     return (
         <Grid textAlign='center' style={{ height: '100vh' }}>
             <Grid.Column style={{ maxWidth: 450 }}>
-                <Image src='/upstorage_logo_730_608.png' size='medium' centered style={{ marginTop: '1em' }} />
+                <Image src={logo} size='medium' centered style={{ marginTop: '1em' }} />
                 <Header as='h2' color='teal' textAlign='center'>
                     Log-in to your account
                 </Header>
@@ -84,7 +79,8 @@ function LoginPage() {
                             iconPosition='left'
                             placeholder='Email'
                             value={authLoginCommand.email}
-                            onChange={(e) => handleInputChange("email", e.target.value)}
+                            onChange={handleInputChange}
+                            name="email"
                         />
                         <Form.Input
                             fluid
@@ -93,14 +89,15 @@ function LoginPage() {
                             placeholder='Password'
                             type='password'
                             value={authLoginCommand.password}
-                            onChange={(e) => handleInputChange("password", e.target.value)}
+                            onChange={handleInputChange}
+                            name="password"
                         />
 
                         <Button color='teal' fluid size='large' type="submit">
                             Login
                         </Button>
 
-                        <Button color='red' fluid onClick={(e) => onGoogleLoginClick(e)} size='large' style={{ marginTop: "5px" }}>
+                        <Button color='red' fluid onClick={onGoogleLoginClick} size='large' style={{ marginTop: "5px" }} type="button">
                             <Icon name='google' /> Sign in with Google
                         </Button>
                     </Segment>
@@ -110,4 +107,4 @@ function LoginPage() {
     );
 }
 
-export default LoginPage
+export default LoginPage;
